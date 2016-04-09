a185b9783174ace9ce45e95406436f402c865f23
marcioggs <marcioggs@gmail.com>
2016-04-09 15:51:00 -0300

    Tratamento de erros na chamada à API.

--- www/js/controllers/search-controller.js
+++ www/js/controllers/search-controller.js
@@ -1,21 +1,22 @@
 angular.module('istalker.controllers.search', [])
 
-  .controller('SearchCtrl', function ($scope, $rootScope, $state, ContactService, $ionicPopup) {
+.controller('SearchCtrl', function ($scope, $rootScope, $state, ContactService, $ionicPopup) {
 
-    $rootScope.contact = {};
-    $rootScope.primaryPhotoUrl = '';
-    //TODO: Para agilizar os testes. Remover posteriormente.
-    $rootScope.stalkedEmail = 'marcioggs@gmail.com';
-    
-    $scope.searchTerm = function() {
-      //TODO: Tratar erros da API.
-      ContactService.findContact($scope.stalkedEmail).then(function(contact) {
-        $rootScope.contact = contact;
-        $state.go('result', null, {reload: true});    });
+  $rootScope.contact = {};
+  $rootScope.primaryPhotoUrl = '';
+  //TODO: Para agilizar os testes. Remover posteriormente.
+  $rootScope.stalkedEmail = 'marcioggs@gmail.com';
+
+  $scope.searchTerm = function() {
+    //TODO: Tratar erros da API.
+    ContactService.findContact($scope.stalkedEmail).then(function (contact) {
+      $rootScope.contact = contact;
+      $state.go('result', null, {reload: true});
     })
-    .catch(function(cause) {
+    .catch(function (cause) {
       showErrorPopup(cause);
     });
+  }
 
   function showErrorPopup(message) {
     $ionicPopup.alert({
