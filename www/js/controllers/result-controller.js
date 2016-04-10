16a11f300bb69bc04a3f2c8bf4da4d131d963407
marcioggs <marcioggs@gmail.com>
2016-04-10 09:18:59 -0300

    Abrir URL das redes sociais.

--- www/js/controllers/result-controller.js
+++ www/js/controllers/result-controller.js
@@ -1,13 +1,18 @@
 angular.module('istalker.controllers.results', [])
 
-  .controller('ResultCtrl', function ($scope, $rootScope, ContactService) {
+.controller('ResultCtrl', function ($scope, $rootScope, ContactService) {
 
+  $scope.primaryPhotoUrl = function () {
+   return ContactService.getPrimaryPhotoURL($rootScope.contact);
+  }
 
-    $scope.primaryPhotoUrl = function () {
-     return ContactService.getPrimaryPhotoURL($rootScope.contact);
-    }
+  $scope.socialNetworkIconURL = function(typeId) {
+    return ContactService.getSocialNetworkIconURL(typeId);
+  }
 
-    $scope.socialNetworkIconURL = function(typeId) {
-      return ContactService.getSocialNetworkIconURL(typeId);
-    }
-  });
+  //TODO: Ver como herdar de um escopo da aplicação.
+  $scope.openUrl = function(url) {
+    window.open(url, "_blank", "location=yes");
+  }
+
+});
