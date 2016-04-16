4aff385d40aa21fa5ae6829ed2c31b0a6124ca46
marcioggs <marcioggs@gmail.com>
2016-04-16 14:33:45 -0300

    Exibir loading automaticamente quando houver requisição http.

--- www/js/controllers/app-controller.js
+++ www/js/controllers/app-controller.js
@@ -1,6 +1,6 @@
 angular.module('istalker.controllers.app', [])
 
-.controller('AppCtrl', function ($scope, $ionicPopup, $ionicLoading) {
+.controller('AppCtrl', function ($scope, $ionicPopup, $ionicLoading, $http) {
 
   $scope.showModalPopup = function(title, message) {
     $ionicPopup.alert({
@@ -13,11 +13,18 @@ angular.module('istalker.controllers.app', [])
     window.open(url, '_blank', 'location=yes');
   }
 
-  $scope.showLoadingUntilStateChange = function() {
-    $ionicLoading.show({
-      template: '<ion-spinner></ion-spinner>',
-      hideOnStateChange: true
-    });
-  }
+  $scope.isRequestingData = function() {
+    return $http.pendingRequests.length > 0;
+  };
+
+  $scope.$watch($scope.isRequestingData, function(isRequestingData) {
+    if(isRequestingData){
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner>'
+      });
+    }else{
+      $ionicLoading.hide();
+    }
+  });
 
 });
